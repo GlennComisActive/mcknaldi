f89864d179beaf6637dbffbac786e204136200d7
jensschilder <34680563+jensschilder@users.noreply.github.com>
2018-07-13 15:29:01 +0200

    Vertaling Backend

--- mcknaldi/Models/Deliveryslots.cs
+++ mcknaldi/Models/Deliveryslots.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,17 @@ namespace mcknaldi.Models
     public class Deliveryslots
     {
         public int Id { get; set; }
+
+        [Display(Name = "Datum")]
         public DateTime DateSlot { get; set; }
+
+        [Display(Name = "Begintijd")]
         public string StartTime { get; set; }
+
+        [Display(Name = "Eindtijd")]
         public string EndTime { get; set; }
+
+        [Display(Name = "Prijs")]
         public decimal Price { get; set; }
     }
 }
